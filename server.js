5e37594a23a19196a5f74c944603eb6bdd4690fe
vischugh <vishal@rebuscode.com>
2017-07-23 00:01:52 +0530

    changes to port in server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var express = require('express');
 var app = express();
 var os = require('os');
 var bodyParser = require('body-parser');
-//var parseUrlencoded = bodyParser.urlencoded({extended: false});
-var port = process.env.PORT_NUMBER || 5000;
 //var passport = require('passport');
 //var LocalStrategy = require('passport-local').Strategy;
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+
+app.set('port', (process.env.PORT || 5000));
 
 //Mongoose database
 var mongo = require('mongodb');
@@ -67,5 +67,6 @@ app.use('/atc', atRouting);
 
 //app.post('/signin', passport.auth);
 
-app.listen(port, os.hostname);
-console.log("Server listening at port" + port + "\nMake requests as localhost:" + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
